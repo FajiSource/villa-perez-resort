e5ee2a94593bede321635cec146771561a419867
faji <yellyhaze000@gmail.com>
2025-11-01 18:43:02 +0800

    services

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,50 +1,18 @@
-import { Button } from "@mui/material";
-export default function Services() {
+import serviceImage from '../assets/beach-bg.jpg'
+function Services() {
     return (
-        <section className="w-full h-screen flex items-center justify-center flex-col text-black gap-10">
-            <div>
-                <div className="text-left mb-10 max-w-xs">
-                    <span className="text-sm flex flex-row items-center gap-3 font-medium">Villa Perez<span className="border-b-2  border-rose w-[50px]"></span></span>
-                    <span className="text-2xl font-medium ">The Most Memorable Rest Time Starts Here.</span>
-                </div>
-
-                <div className="flex flex-row justify-evenly gap-10">
-                    <div className="w-72 bg-white h-auto relative text-black rounded-lg shadow-2xl">
-                        <div className="w-full h-1/2 relative rounded-t-lg">
-                            <img src="/images/cottage-sample.jpg" alt="" className="w-full h-full rounded-t-lg" />
-                        </div>
-                        <div className="h-1/2 w-full p-3">
-                            <span className="font-medium text-sm">Rooms</span>
-                            <p className="text-xs text-black/50">Lorem ipsum dolor sit apiciatis, libero consequuntur, dolore et nostrum minus, hic quae! Ullam facere quis deleniti repellendus doloremque.</p>
-                            <span className="text-xs text-black/50 block mb-2 mt-2 ">Starting from <span className="text-black font-medium">2,600 Pesos</span></span>
-                            <Button variant="contained" className="">Book Now</Button>
-                        </div>
-                    </div>
-                    <div className="w-72 bg-white h-auto relative text-black rounded-lg shadow-2xl">
-                        <div className="w-full h-1/2 relative rounded-t-lg">
-                            <img src="/images/cottage-sample.jpg" alt="" className="w-full h-full rounded-t-lg" />
-                        </div>
-                        <div className="h-1/2 w-full p-3">
-                            <span className="font-medium text-sm">Rooms</span>
-                            <p className="text-xs text-black/50">Lorem ipsum dolor sit apiciatis, libero consequuntur, dolore et nostrum minus, hic quae! Ullam facere quis deleniti repellendus doloremque.</p>
-                            <span className="text-xs text-black/50 block mb-2 mt-2 ">Starting from <span className="text-black font-medium">2,600 Pesos</span></span>
-                            <Button variant="contained" className="">Book Now</Button>
-                        </div>
-                    </div>
-                    <div className="w-72 bg-white h-auto relative text-black rounded-lg shadow-2xl">
-                        <div className="w-full h-1/2 relative rounded-t-lg">
-                            <img src="/images/cottage-sample.jpg" alt="" className="w-full h-full rounded-t-lg" />
-                        </div>
-                        <div className="h-1/2 w-full p-3">
-                            <span className="font-medium text-sm">Rooms</span>
-                            <p className="text-xs text-black/50">Lorem ipsum dolor sit apiciatis, libero consequuntur, dolore et nostrum minus, hic quae! Ullam facere quis deleniti repellendus doloremque.</p>
-                            <span className="text-xs text-black/50 block mb-2 mt-2 ">Starting from <span className="text-black font-medium">2,600 Pesos</span></span>
-                            <Button variant="contained" className="">Book Now</Button>
-                        </div>
-                    </div>
+        <section className='w-full h-screen'>
+            <div className='w-full h-1/2 bg-center bg-cover'
+                style={{ backgroundImage: `url(${serviceImage})` }}>
+                <div>
 
                 </div>
+            </div>
+            <div className='w-full h-1/2'>
+
             </div>
         </section>
     )
 }
+
+export default Services
